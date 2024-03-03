5e628a1238d4d4b9cb2417d81ac0423bf521200b
manur02 <manur02@pfw.edu>
2024-03-03 17:41:47 -0500

    Feature2-Subtask2-Push

--- Features2.py
+++ Features2.py
@@ -3,3 +3,14 @@ class TeamMember:
         self.hours_off = hours_off
         self.hours_committed = hours_committed
         self.available_hours = available_hours
+
+    def calculate_available_hours(self, sprint_days):
+        """
+        Calculate available effort-hours for the team member.
+        :param sprint_days: Number of sprint days
+        :return: Available effort-hours for the team member
+
+        """
+        daily_hours_min, daily_hours_max = self.available_hours
+        total_available_hours = (sprint_days - self.hours_off) * (daily_hours_max - self.hours_committed)
+        return total_available_hours
